1ebe84c15fc78c5af593bd505caf7510964f0464
Alexey <avbolshakov@gmail.com>
2023-11-19 11:01:37 +0300

    Добавил аутентификацию по токенам

--- lk/lk/settings.py
+++ lk/lk/settings.py
@@ -128,3 +128,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1",
+]
